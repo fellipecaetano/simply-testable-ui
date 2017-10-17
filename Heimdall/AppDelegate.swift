bf2b52dde6ea0b3e5967b39c8aaa0cd001b8aa12
Fellipe Caetano <fellipe.caetano4@gmail.com>
2017-10-17 20:44:52 -0200

    Add a root view controller

--- Heimdall/AppDelegate.swift
+++ Heimdall/AppDelegate.swift
@@ -2,9 +2,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = .white
+        window?.makeKeyAndVisible()
         return true
     }
 }
