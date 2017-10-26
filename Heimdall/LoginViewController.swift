fc91bd0bf5d28a6a3fc37fd2d768b36a19dab4cc
Fellipe Caetano <fellipe.caetano4@gmail.com>
2017-10-25 23:39:41 -0200

    Remove LoginView
    Outlets are then bound to the view controller directly.

--- Heimdall/LoginViewController.swift
+++ Heimdall/LoginViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+    // Outlets
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var emailValidationLabel: UILabel!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordValidationLabel: UILabel!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
